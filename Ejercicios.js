9d9d83db524706e4c9dd8507319a20f7de127b89
Noelia <noelianarduzzi@gmail.com>
2023-04-03 22:27:33 -0300

    Ejercicio1

---
+++ Ejercicios.js
@@ -0,0 +1,16 @@
+const personajesSimpsons = [
+    { nombre: "Homero", edad: 39, rol: "Padre" },
+    { nombre: "Marge", edad: 36, rol: "Madre" },
+    { nombre: "Bart", edad: 10, rol: "Hijo" },
+    { nombre: "Lisa", edad: 8, rol: "Hija" },
+    { nombre: "Maggie", edad: 1, rol: "Hija" },
+    { nombre: "Ned", edad: 35, rol: "Vecino" },
+    { nombre: "Ralph", edad: 8, rol: "Compañero de clase" },
+    { nombre: "Milhouse", edad: 10, rol: "Amigo" },
+    { nombre: "Nelson", edad: 12, rol: "Bravucón" },
+    { nombre: "Martin", edad: 10, rol: "Compañero de clase" }
+    ];
+
+    const menoresDe18 = personajesSimpsons.filter(personajesSimpsons => personajesSimpsons.edad < 18);
+    
+    console.log(menoresDe18);
